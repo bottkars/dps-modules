f94b7086350e6b162b10149f62e5dc2e32658120
bottkars <karsten.bott@bottnet.de>
2020-07-07 15:00:51 +0200

    token function

--- ci/functions/ppdm_functions.sh
+++ ci/functions/ppdm_functions.sh
@@ -2,7 +2,7 @@
 
 function get_token {
     local password=$1
-    local TOKEN=$(curl -k -sS --request POST \
+    local token=$(curl -k -sS --request POST \
     --connect-timeout 10 \
     --max-time 10 \
     --retry 3 \
@@ -11,7 +11,7 @@ function get_token {
     --url "https://${PPDM_FQDN}:8443/api/v2/login" -k \
     --header 'content-type: application/json' \
     --data '{"username":"admin","password":"'${password}'"}' | jq -r .access_token )
-
+    echo $token
 }
 
 
