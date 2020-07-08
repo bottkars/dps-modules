eb04c361e057b1a942389f01e32ebcae0011a800
bottkars <karsten.bott@bottnet.de>
2020-07-08 06:58:35 +0200

    changed to inventory

--- ci/scripts/set-ppdm-inventory.sh
+++ ci/scripts/set-ppdm-inventory.sh
@@ -9,23 +9,23 @@ echo "requesting API token"
 
 TOKEN=$(get_ppdm_token ${PPDM_PASSWORD})
 
-echo "Creating DDVE Credentials for ${DDVE_USERNAME}"
-CREDENTIALS=$(create_ppdm_credentials ${TOKEN} DATADOMAIN ${DDVE_USERNAME} ${DDVE_PASSWORD})
+echo "Creating INVENTORY Credentials for ${INVENTORY_USERNAME}"
+CREDENTIALS=$(create_ppdm_credentials ${TOKEN} ${INVENTORY_CREDENTIAL_TYPE} ${INVENTORY_USERNAME} ${INVENTORY_PASSWORD})
 CREDENTIALS_ID=$(echo $CREDENTIALS | jq -r '.id')
 
-CERTIFICATE=$(get_ppdm_host_certificate $TOKEN $DDVE_FQDN)
+CERTIFICATE=$(get_ppdm_host_certificate $TOKEN $INVENTORY_FQDN)
 CERTIFICATE=$(echo $CERTIFICATE | jq -r '.[]' )
 CERTIFICATE=$(echo $CERTIFICATE | jq '(.state |= "ACCEPTED")' )
 CERT_ID=$(echo $CERTIFICATE | jq -r '.id')
 
-echo "Trusting DDVE ${DDVE_FQDN} certificate $CERTIFICATE"
+echo "Trusting INVENTORY ${INVENTORY_FQDN} certificate $CERTIFICATE"
 trust_ppdm_host_certificate "${TOKEN}" "${CERTIFICATE}" "${CERT_ID}"
 
-echo "Adding DDVE ${DDVE_FQDN} to inventory"
-DDVE_NAME=$(echo ${DDVE_FQDN} | cut -d '.' -f-1  )
+echo "Adding INVENTORY ${INVENTORY_FQDN} to inventory"
+INVENTORY_NAME=$(echo ${INVENTORY_FQDN} | cut -d '.' -f-1  )
 echo "token: $(echo $TOKEN | base64 ) 
-type:EXTERNALDATADOMAIN 
+type: $INVENTORY_TYPE 
 ddve_name: ${DDVE_NAME}
 ddve_fqdn: ${DDVE_FQDN} 
 cred_id: $CREDENTIALS_ID"
-create_ppdm_inventory_source $TOKEN EXTERNALDATADOMAIN ${DDVE_NAME} "${DDVE_FQDN}" $CREDENTIALS_ID
+create_ppdm_inventory_source $TOKEN $INVENTORY_TYPE ${DDVE_NAME} "${DDVE_FQDN}" $CREDENTIALS_ID
