3eded987a53396737fac315d290f31f71ec523a3
bottkars <karsten.bott@bottnet.de>
2020-07-09 17:46:13 +0200

    ddce inventory

--- ci/scripts/set-ppdm-inventory.sh
+++ ci/scripts/set-ppdm-inventory.sh
@@ -25,5 +25,4 @@ trust_ppdm_host_certificate  "${CERTIFICATE}" "${CERT_ID}"
 
 echo "Adding INVENTORY ${INVENTORY_FQDN} to inventory"
 INVENTORY_NAME=$(echo ${INVENTORY_FQDN} | cut -d '.' -f-1  )
-result=$(create_ppdm_inventory-source $INVENTORY_TYPE ${INVENTORY_NAME} "${INVENTORY_FQDN}" $CREDENTIALS_ID ${INVENTORY_PORT})
-echo $result 
+create_ppdm_inventory-source $INVENTORY_TYPE ${INVENTORY_NAME} "${INVENTORY_FQDN}" $CREDENTIALS_ID ${INVENTORY_PORT}
