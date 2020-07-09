682c0b4654f0301b7ca3285d84e8706362cd767b
bottkars <karsten.bott@bottnet.de>
2020-07-09 16:35:57 +0200

    dr

--- ci/scripts/set-ppdm-sdr.sh
+++ ci/scripts/set-ppdm-sdr.sh
@@ -8,6 +8,8 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 source dps_modules/ci/functions/ppdm_functions.sh
 echo "requesting API token"
 
+export PPDM_TOKEN=$(get_ppdm_token ${PPDM_PASSWORD})
+echo "Configuring Server DR"
 
 
 set_ppdm_sdr-settings "${DDVE_FQDN}" "${DDVE_PATH}" 'DATA_DOMAIN_SYSTEM' 'true'
