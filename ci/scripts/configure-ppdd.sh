5fa58985314e974dcec4be1edadd708ab31754a4
bottkars <karsten.bott@bottnet.de>
2020-07-14 08:54:06 +0200

    fqdn

--- ci/scripts/configure-ppdd.sh
+++ ci/scripts/configure-ppdd.sh
@@ -16,8 +16,7 @@ ddsh net config ${DDVE_INTERFACE} dhcp no
 ddsh net config ${DDVE_INTERFACE} type fixed ${DDVE_ADDRESS} netmask ${DDVE_NETMASK}
 ddsh net route add gateway ${DDVE_GATEWAY}
 ddsh net set dns ${DDVE_DNS}
-ddsh net set hostname ${DDVE_HOSTNAME}
+ddsh net set hostname ${DDVE_FQDN}
 ddsh net set searchdomain  ${DDVE_DOMAIN}
 ddsh elicense reset restore-evaluation
-break
 
