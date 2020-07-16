def5f1a9eb340aae1d8182dfa761410b6f3011b1
bottkars <karsten.bott@bottnet.de>
2020-07-16 17:06:34 +0200

    adding prefix to not empty var

--- ci/scripts/add-ppdm-vproxy.sh
+++ ci/scripts/add-ppdm-vproxy.sh
@@ -7,16 +7,16 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 source dps_modules/ci/functions/ppdm_functions.sh
 source dps_modules/ci/functions/yaml.sh
 
-create_variables "${PROXY_FILE}"
+create_variables "${PROXY_FILE}" sourced_
 export PPDM_TOKEN=$(get_ppdm_token $PPDM_PASSWORD)
-printf "evaluating Moref for ${vsphere_host} ... "
-ClusterMoref=$(govc host.info -json ${vsphere_host} | jq -r '.HostSystems[0].Parent.Value')
+printf "evaluating Moref for ${sourced_vsphere_host} ... "
+ClusterMoref=$(govc host.info -json ${sourced_vsphere_host} | jq -r '.HostSystems[0].Parent.Value')
 echo $ClusterMoref
-printf "evaluating Moref for ${vsphere_portgroup} ${vsphere_vswitch} ... "
-NetworkMoref=$(govc dvs.portgroup.info -json -pg ${vsphere_portgroup} ${vsphere_vswitch} | jq -r '.Port[0].PortgroupKey')
+printf "evaluating Moref for ${sourced_vsphere_portgroup} ${sourced_vsphere_vswitch} ... "
+NetworkMoref=$(govc dvs.portgroup.info -json -pg ${sourced_vsphere_portgroup} ${sourced_vsphere_vswitch} | jq -r '.Port[0].PortgroupKey')
 echo $NetworkMoref
-printf "evaluating Moref for ${vsphere_datastore} ... "
-DatastoreMoref=$(govc datastore.info -json ${vsphere_datastore}  | jq -r '.Datastores[].Self.Value')
+printf "evaluating Moref for ${sourced_vsphere_datastore} ... "
+DatastoreMoref=$(govc datastore.info -json ${sourced_vsphere_datastore}  | jq -r '.Datastores[].Self.Value')
 echo $DatastoreMoref
 
 protection_engine_id=$(get_ppdm_protection-engines | jq -r .id)
@@ -27,11 +27,11 @@ add_ppdm_protection_engine_proxy  \
     "${NetworkMoref}" \
     "${ClusterMoref}" \
     "${DatastoreMoref}" \
-    "${Fqdn}" \
-    "${IpAddress}" \
-    "${NetMask}" \
-    "${Gateway}" \
-    "${Dns}" \
-    "${IPProtocol}" \
-    "${HostName}" \
+    "${sourced_Fqdn}" \
+    "${sourced_IpAddress}" \
+    "${sourced_NetMask}" \
+    "${sourced_Gateway}" \
+    "${sourced_Dns}" \
+    "${sourced_IPProtocol}" \
+    "${sourced_HostName}" \
     "${VimServerRefID}"
