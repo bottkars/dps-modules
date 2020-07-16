af731a32c8d5bf724cd4c98ce574d1c11b531cc0
bottkars <karsten.bott@bottnet.de>
2020-07-16 16:48:28 +0200

    latest

--- ci/scripts/add-ppdm-vproxy.sh
+++ ci/scripts/add-ppdm-vproxy.sh
@@ -9,13 +9,19 @@ source dps_modules/ci/functions/yaml.sh
 
 create_variables "${PROXY_FILE}"
 export PPDM_TOKEN=$(get_ppdm_token $PPDM_PASSWORD)
-
+printf "evaluating Moref for ${vsphere_host} ... "
 ClusterMoref=$(govc host.info -json ${vsphere_host} | jq -r '.HostSystems[0].Parent.Value')
+echo $ClusterMoref
+printf "evaluating Moref for ${vsphere_portgroup} ${vsphere_vswitch} ... "
 NetworkMoref=$(govc dvs.portgroup.info -json -pg ${vsphere_portgroup} ${vsphere_vswitch} | jq -r '.Port[0].PortgroupKey')
+echo $NetworkMoref
+printf "evaluating Moref for ${vsphere_datastore} ... "
 DatastoreMoref=$(govc datastore.info -json ${vsphere_datastore}  | jq -r '.Datastores[].Self.Value')
+echo $DatastoreMoref
 
 protection_engine_id=$(get_ppdm_protection-engines | jq -r .id)
 VimServerRefID=$(get_ppdm_inventory-sources  | jq -r 'select(.address==env.GOVC_URL) | .id')
+
 add_ppdm_protection_engine_proxy  \
     "${protection_engine_id}" \
     "${NetworkMoref}" \
@@ -29,4 +35,3 @@ add_ppdm_protection_engine_proxy  \
     "${IPProtocol}" \
     "${HostName}" \
     "${VimServerRefID}"
-
