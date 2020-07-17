6bd5468bbd38e898b63e3d281bc7ae6590e816f7
bottkars <karsten.bott@bottnet.de>
2020-07-17 07:04:44 +0200

    run command as eval

--- ci/scripts/add-ppdm-vproxy.sh
+++ ci/scripts/add-ppdm-vproxy.sh
@@ -32,7 +32,7 @@ echo $FolderMoref
 
 protection_engine_id=$(get_ppdm_protection-engines | jq -r .id)
 VimServerRefID=$(get_ppdm_inventory-sources  | jq -r 'select(.address==env.GOVC_URL) | .id')
-request=add_ppdm_protection_engine_proxy  \
+request=$(add_ppdm_protection_engine_proxy \
     "${protection_engine_id}" \
     "${NetworkMoref}" \
     "${ClusterMoref}" \
@@ -45,5 +45,6 @@ request=add_ppdm_protection_engine_proxy  \
     "${sourced_Dns}" \
     "${sourced_IPProtocol}" \
     "${sourced_VMName}" \
-    "${VimServerRefID}" 
+    "${VimServerRefID}" \
+    )
 echo $request | jq -r .
