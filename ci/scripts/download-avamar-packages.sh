4ffbf0f2c956bebf9c5a5e6445bb3a824c075ba9
bottkars <karsten.bott@bottnet.de>
2020-07-20 15:50:56 +0200

    array iteration

--- ci/scripts/download-avamar-packages.sh
+++ ci/scripts/download-avamar-packages.sh
@@ -10,10 +10,10 @@ AV_BASEURL=$(echo $avamar | jq -r ".BaseURL")
 AV_VERSION=$(echo $avamar | jq -r ".version")
 # ftp://avamar_ftp:anonymous@ftp.emc.com/software/av3494954704/19.2.0.155.vhd.7z
 # ftp://avamar_ftp:anonymous@ftp.emc.com/software/av3494954704/19.2.0.155.ova
-for PACKAGE in $packages
+for PACKAGE in ${packages[@]}
 do
     echo "downloading ${PACKAGE} ${AV_RELEASE}"
     echo "from ${AV_BASEURL}/${AV_LOCATION}/${PACKAGE}-${AV_RELEASE}.${extension}"
-    curl --fail --disable-epsv -o ${PACKAGE}/${PACKAGE}-${AV_RELEASE}.${extension} ${AV_BASEURL}/${AV_LOCATION}/${PACKAGE}-${AV_RELEASE}.${extension}
+    curl --disable-epsv -o ${PACKAGE}/${PACKAGE}-${AV_RELEASE}.${extension} ${AV_BASEURL}/${AV_LOCATION}/${PACKAGE}-${AV_RELEASE}.${extension}
 done
 
