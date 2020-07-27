3fbb3934d454e846bb5ae233d10ebe851f1cc86f
bottkars <karsten.bott@bottnet.de>
2020-07-27 13:34:12 +0200

    plan environment

--- ci/scripts/terraform-show-plan.sh
+++ ci/scripts/terraform-show-plan.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eu
-[[ "${DEBUG}" == "TRUE" ]] && set -x
+# [[ "${DEBUG}" == "TRUE" ]] && set -x
+echo "Creating Plan Envrionment"
+mv plan-output-archive/terraform/.t* plan-output-archive/terraform/${STATE_OUTPUT_DIR}/
+cd plan-output-archive/terraform/${STATE_OUTPUT_DIR}/
+echo "Verifyiing Plan Envrionment"
 
-exit 1
+terrafrom show .tfplan
