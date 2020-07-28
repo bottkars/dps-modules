546b3cf96abaf437a6fc095879b0248ecf47ca89
bottkars <karsten.bott@bottnet.de>
2020-07-28 11:47:05 +0200

    ave test 1

--- ci/functions/govc_functions.sh
+++ ci/functions/govc_functions.sh
@@ -3,6 +3,7 @@
 function create_disk {
     local disk_name=${1}
     local disk_size=${2}
+    local vm_name=$(echo $GOVC_VM_IPATH | rev |cut -d '/' -f1 | rev)
     govc vm.disk.create -vm.ipath "${GOVC_VM_IPATH}" \
--name "$PPDD_VMNAME/${disk_name}" -size ${disk_size}
+-name "$vm_name/${disk_name}" -size ${disk_size}
 }
--- ci/scripts/deploy-ave.sh
+++ ci/scripts/deploy-ave.sh
@@ -3,7 +3,7 @@ set -eu
 [[ "${DEBUG}" == "TRUE" ]] && set -x
 figlet DPS Automation
 AVE_VERSION=$(cat avamar/version)
-echo "preparing avamar ${AVE_VERSION} nve"
+echo "preparing avamar ${AVE_VERSION} Virtual Edition"
 
 govc about
 
@@ -11,20 +11,20 @@ echo "checking for jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 govc import.spec avamar/AVE-${AVE_VERSION}.ova > avamar.json
 echo "configuring appliance (vami) settings"
+source dps_modules/ci/functions/govc_functions.sh
 
-break 1
 
-sleep 10000
+jq  '(.DiskProvisioning |= "thin")' avamar.json  > "tmp" && mv "tmp" avamar.json
 
-jq  '(.PropertyMapping[] | select(.Key == "vami.ipv4.NetWorker_Virtual_Edition") | .Value) |= env.AVE_ADDRESS' avamar.json > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.gatewayv4.NetWorker_Virtual_Edition") | .Value) |= env.AVE_GATEWAY' avamar.json > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.DNS.NetWorker_Virtual_Edition") | .Value) |= env.AVE_DNS' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.searchpaths.NetWorker_Virtual_Edition") | .Value) |= env.AVE_SEARCHPATHS' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.AVEtimezone.NetWorker_Virtual_Edition") | .Value) |= env.AVE_TIMEZONE' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.NTP.NetWorker_Virtual_Edition") | .Value) |= env.AVE_NTP' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterFQDN.NetWorker_Virtual_Edition") | .Value) |= env.GOVC_URL' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterUsername.NetWorker_Virtual_Edition") | .Value) |= env.GOVC_USERNAME' avamar.json  > "tmp" && mv "tmp" avamar.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.FQDN.NetWorker_Virtual_Edition") | .Value) |= env.AVE_FQDN' avamar.json  > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.ipv4.Avamar_Virtual_Edition") | .Value) |= env.AVE_ADDRESS' avamar.json > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.gatewayv4.Avamar_Virtual_Edition") | .Value) |= env.AVE_GATEWAY' avamar.json > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.DNS.Avamar_Virtual_Edition") | .Value) |= env.AVE_DNS' avamar.json  > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.searchpaths.Avamar_Virtual_Edition") | .Value) |= env.AVE_SEARCHPATHS' avamar.json  > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.AVEtimezone.Avamar_Virtual_Edition") | .Value) |= env.AVE_TIMEZONE' avamar.json  > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.NTP.Avamar_Virtual_Edition") | .Value) |= env.AVE_NTP' avamar.json  > "tmp" && mv "tmp" avamar.json
+# jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterFQDN.Avamar_Virtual_Edition") | .Value) |= env.GOVC_URL' avamar.json  > "tmp" && mv "tmp" avamar.json
+# jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterUsername.Avamar_Virtual_Edition") | .Value) |= env.GOVC_USERNAME' avamar.json  > "tmp" && mv "tmp" avamar.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.FQDN.Avamar_Virtual_Edition") | .Value) |= env.AVE_FQDN' avamar.json  > "tmp" && mv "tmp" avamar.json
 jq  '(.NetworkMapping[].Name |= env.AVE_NETWORK)' avamar.json  > "tmp" && mv "tmp" avamar.json
 # jq  '(.NetworkMapping[].Network |= "ethernet-0")' avamar.json  > "tmp" && mv "tmp" avamar.json
 # jq  '(.PowerOn |= false)' avamar.json  > "tmp" && mv "tmp" avamar.json
@@ -34,13 +34,73 @@ echo "importing avamar ${AVE_VERSION} AVE template"
 govc import.ova -name ${AVE_VMNAME} -folder=${AVE_FOLDER}  -options=avamar.json avamar/AVE-${AVE_VERSION}.ova
 govc vm.network.change -vm.ipath ${GOVC_VM_IPATH} -net=VLAN250 ethernet-0
 
-govc vm.power -on=true -vm.ipath ${GOVC_VM_IPATH}
-echo "finished DELLEMC Networker  ${AVE_VERSION} AVE install"
-echo "Waiting for AVE avi-installer to bevome ready, this can take up to 5 Minutes"
-until [[ 200 == $(curl -k --write-out "%{http_code}\n" --silent --output /dev/null "https://${AVE_FQDN}:443/avi/avigui.html") ]] ; do
-    printf '.'
-    sleep 5
+
+case "${AVE_SIZE}" in
+    "0.5TB")       
+    AVE_CPU=2
+    AVE_MEM=6144
+    AVE_DISK_SIZE=250G
+    AVE_DISK_COUNT=3
+    ;;
+    "1TB")
+    AVE_CPU=2
+    AVE_MEM=8192
+    AVE_DISK_SIZE=250G
+    AVE_DISK_COUNT=6
+    ;;            
+    "2TB")
+    AVE_CPU=2
+    AVE_MEM=16384
+    AVE_DISK_SIZE=1000G
+    AVE_DISK_COUNT=3
+    ;;
+    "4TB")
+    AVE_CPU=4
+    AVE_MEM=36864
+    AVE_DISK_SIZE=1000G
+    AVE_DISK_COUNT=6
+    ;;  
+    "8TB")
+    AVE_CPU=8
+    AVE_MEM=49125
+    AVE_DISK_SIZE=1000G
+    AVE_DISK_COUNT=12
+    ;;  
+    "16TB")
+    AVE_CPU=16
+    AVE_MEM=99304
+    AVE_DISK_SIZE=2000G
+    AVE_DISK_COUNT=12
+    ;;  
+    *)
+    break
+esac 
+
+echo "Setting AVE to  
+    ${AVE_CPU} CPU
+    ${AVE_MEM}MB Memory
+    ${AVE_DISK_SIZE}B Disk Size
+    ${AVE_DISK_COUNT} Disk Count
+    "
+
+govc vm.change -vm.ipath ${GOVC_VM_IPATH} -m=${AVE_MEM} -mem.reservation=1024
+# looping the disk creation
+i=1
+until [ $i -gt $AVE_DISK_COUNT ]; do
+   echo "Creating disk${i} with ${AVE_DISK_SIZE}B"
+   create_disk "gsandisk${i}" "${AVE_DISK_SIZE}"
+   let i++
 done
-echo
-echo "Appliance https://${AVE_FQDN}:443/avi/avigui.html is ready for Configuration with root:changeme"
+
+
+govc vm.power -on=true -vm.ipath ${GOVC_VM_IPATH}
+echo "finished DELLEMC Avamar  ${AVE_VERSION} Virtual Edition install"
+
+#echo "Waiting for AVE avi-installer to bevome ready, this can take up to 5 Minutes"
+#until [[ 200 == $(curl -k --write-out "%{http_code}\n" --silent --output /dev/null "https://${AVE_FQDN}:443/avi/avigui.html") ]] ; do
+#    printf '.'
+#    sleep 5
+#done
+#echo
+#echo "Appliance https://${AVE_FQDN}:443/avi/avigui.html is ready for Configuration with root:changeme"
 
