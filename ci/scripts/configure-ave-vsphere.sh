56aaa0d99eb7c5e9784cc6f118fce3cb5feee85e
bottkars <karsten.bott@bottnet.de>
2020-07-29 13:36:58 +0200

    ave

---
+++ ci/scripts/configure-ave-vsphere.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+[[ "${DEBUG}" == "TRUE" ]] && set -x
+figlet DPS Automation
+
+  
+
+
+
+WORKFLOW=AveConfig
+echo "waiting for AVAMAR $WORKFLOW  to be available"
+### get the SW Version
+until [[ ! -z $AVE_CONFIG ]]
+do
+AVE_CONFIG=$( /opt/emc-tools/bin/avi-cli --user root --password "${AVE_SETUP_PASSWORD}" \
+ --listrepository localhost 2> /dev/null  \
+ | grep ${WORKFLOW} | awk  '{print $1}' )
+sleep 5
+printf "."
+done
+
+
+echo "waiting for ave-config to become ready"
+until [[ $(/opt/emc-tools/bin/avi-cli --user root --password "${AVE_SETUP_PASSWORD}" \
+ --listhistory localhost | grep ave-config | awk  '{print $5}') == "ready" ]]
+do
+printf "."
+sleep 5
+done
+
+
+
+AVE_TIMEZONE="Europe/Berlin"
+AVE_COMMON_PASSWORD="Change_Me12345_"
+/opt/emc-tools/bin/avi-cli --user root --password "${AVE_PASSWORD}" --install ave-config  \
+    --input timezone_name="${AVE_TIMEZONE}" \
+    --input common_password=${AVE_COMMON_PASSWORD} \
+    --input use_common_password=true \
+    --input repl_password=${AVE_COMMON_PASSWORD} \
+    --input rootpass=${AVE_COMMON_PASSWORD} \
+    --input mcpass=${AVE_COMMON_PASSWORD} \
+    --input viewuserpass=${AVE_COMMON_PASSWORD} \
+    --input admin_password_os=${AVE_COMMON_PASSWORD} \
+    --input root_password_os=${AVE_COMMON_PASSWORD} \
+    localhost
+#until [[ 200 == $(curl -k --write-out "%{http_code}\n" --silent --output /dev/null "https://${NVE_FQDN}:9000") ]] ; do
+#    printf '.'
+#    sleep 5
+#done
+
+#echo
+#echo "Networker Appliance https://${NVE_FQDN}:9000 is ready !"
+
+## validate new_ddboost_user over ddboost_user
+
