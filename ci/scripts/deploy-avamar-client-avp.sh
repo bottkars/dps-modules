3bcdf2b12cec46bd2b1b2d29ab3a142858ad7a3c
bottkars <karsten.bott@bottnet.de>
2020-07-30 14:12:24 +0200

    check avp

--- ci/scripts/deploy-avamar-client-avp.sh
+++ ci/scripts/deploy-avamar-client-avp.sh
@@ -8,17 +8,14 @@ source dps_modules/ci/functions/ave_functions.sh
 AVP_VERSION=$(echo $AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE  | cut -d "-" -f2-)
 AVP_VERSION=${AVP_VERSION//.avp}
 AVP_VERSION=${AVP_VERSION/-/.}
-
+echo "Checking if ${AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE} is already installed"
 set +e
 if [[ $AVP_VERSION == "$(avi-cli-run --user root --password "${AVE_PASSWORD}" --listhistory localhost  | grep upgrade-client-downloads |  awk '{print $3}')" ]]
     then
         echo "${AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE} already installed, nothing to do here"
     else
         set -e
-        avi-cli-run --user root --password "${AVE_PASSWORD}" --listhistory localhost  | grep upgrade-client-downloads
-
         echo "Downloading ${AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE} to avamar Server Repo"
-
         avi-run-bashscript curl -k "'${AVE_UPGRADE_CLIENT_DOWNLOADS_URL}'" --output /space/avamar/repo/packages/${AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE}
 
         echo "Waiting for Package ${AVE_UPGRADE_CLIENT_DOWNLOADS_PACKAGE} to become available for Installation"
