50582ee60aaaa33ded411f8961cc474077414d72
“bottkars” <Karsten.bott@bottnet.de>
2020-09-01 09:54:21 +0200

    added nve rest config

---
+++ ci/scripts/configure-nve-rest.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+set -e
+[[ "${DEBUG}" == "TRUE" ]] && set -x
+figlet DPS Automation
+
+source dps-modules/ci/functions/avi_functions.sh
+export WORKFLOW=ave-config
+printf "Configuring Networker Virtual Edition\n"
+printf "testing we can resolve the AVI at %s" "${AVI_FQDN}"
+until dig +short -t srv "${AVI_FQDN}"; do
+    printf '.'
+    sleep 5
+done
+
+printf "Waiting for AVI System to become ready"
+until [[ 200 == $(curl -k --write-out "%{http_code}\n" --silent --output /dev/null "https://${AVI_FQDN}:443/avi/avigui.html") ]] ; do
+    printf '.'
+    sleep 5
+done
+printf "\n"
+
+
+AVI_TOKEN=$(get_avi_token $NVE_PASSWORD)
+
+
+echo "waiting for ${WORKFLOW} to become ready"
+
+until [[ $(get_avi_packages | jq -r 'select(.title==env.WORKFLOW).status == "ready"')  ]]
+do
+sleep 5
+printf "."
+done
+printf "\n"
+printf "Installation System ready, now configuring AVE \n"
+printf "This might take up to 40 Minutes\n"
+
+
+data='{"timezone_name":"'${NVE_TIMEZONE}'", 
+"admin_password_os":"'${NVE_ADMIN_PASSWORD_OS}'" ,
+"root_password_os":"'${NVE_ROOT_PASSWORD_OS}'" ,
+"snmp_string":"'${NVE_SNMP_STRING}'" ,
+"datadomain_host":"'${NVE_DATADOMAIN_HOST:-defaulthost}'" ,
+"storage_path":"'${NVE_STORAGE_PATH:-default}'" ,
+"new_ddboost_user":"'${NVE_NEW_DDBOOST_USER:-false}'" ,
+"ddboost_user":"'${NVE_DDBOOST_USER:-default}'" ,
+"ddboost_user_pwd":"'${NVE_DDBOOST_USER_PWD:-default}'" ,
+"ddboost_user_pwd_cf":"'${NVE_DDBOOST_USER_PWD_CF:-default}'" ,
+"datadomain_sysadmin":"'${NVE_DATADOMAIN_SYSADMIN:-sysadmin}'" ,
+"datadomain_sysadmin_pwd":"'${NVE_DATADOMAIN_SYSADMIN_PWD:-default}'" ,
+"tomcat_keystore_password":"'${NVE_TOMCAT_KEYSTORE_PASSWORD}'" ,
+"authc_admin_password":"'${NVE_AUTHC_ADMIN_PASSWORD}'" ,
+"install_avpasswd":"false" ,
+"add_datadomain_config":"false"
+}'
+
+
+data='{"timezone_name":"'${NVE_TIMEZONE}'", 
+"admin_password_os":"'${NVE_ADMIN_PASSWORD_OS}'" ,
+"root_password_os":"'${NVE_ROOT_PASSWORD_OS}'" ,
+"snmp_string":"'${NVE_SNMP_STRING}'" ,
+"tomcat_keystore_password":"'${NVE_TOMCAT_KEYSTORE_PASSWORD}'" ,
+"authc_admin_password":"'${NVE_AUTHC_ADMIN_PASSWORD}'" ,
+"install_avpasswd":"false" ,
+"add_datadomain_config":"false"
+}'
+
+
+
+data=$(echo "${data}" | jq -c .)
+set_avi_config $data $WORKFLOW| jq -r .
+
+until  [[  $(get_avi_messages | jq -r 'select(.[-1].status == "completed")' 2> /dev/null) ]]
+    do
+    get_avi_messages  | jq -r  '.[-1] | [.timestamp, .status, .taskId, .taskName, .content] | @tsv'
+    sleep 10
+done
+
+
+
+echo
+echo "Done"
