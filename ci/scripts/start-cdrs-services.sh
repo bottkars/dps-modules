af2a1498f79dd7a3f9bce2335565d94d12e9d941
bottkars <karsten.bott@dell.com>
2020-10-19 14:13:42 +0200

    remove figlet

--- ci/scripts/start-cdrs-services.sh
+++ ci/scripts/start-cdrs-services.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eu
 [[ "${DEBUG}" == "TRUE" ]] && set -x
-figlet DPS Automation
-echo "checking for jq...."
 source dps-modules/ci/functions/ppdm_functions.sh
 source dps-modules/ci/functions/yaml.sh
 
