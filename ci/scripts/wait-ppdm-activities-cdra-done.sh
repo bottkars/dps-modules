65c6723a5c265ee3dceb703f2e188a421a7d0103
bottkars <karsten.bott@dell.com>
2020-10-23 08:10:29 +0200

    updated query

--- ci/scripts/wait-ppdm-activities-cdra-done.sh
+++ ci/scripts/wait-ppdm-activities-cdra-done.sh
@@ -9,13 +9,13 @@ export PPDM_TOKEN=$(get_ppdm_token $PPDM_PASSWORD)
 
 # CDRA_STATE_FILE="$(echo "$CDRA_STATE_FILE" | envsubst)" 
 
-while   [[ $(query_ppdm_activities 'CLOUD_PROTECT' | jq -r '.[].state == "RUNNING"') ]] 
+while   [[ $(query_ppdm_activities 'MYSQL1' 'parentId eq null and category in ("CLOUD_PROTECT") and state in ("RUNNING")' | jq -r '.[].state == "RUNNING"') ]] 
 do
     echo "CloudDR Replication running"
     sleep 60
 done 
 
-
+query_ppdm_activities '*' 'parentId eq null and category in ("CLOUD_PROTECT") and state in ("COMPLETED")' | jq -r '.[-1]'
 
 
 
