11cfdd9575f9f219fb97b79d064428db39fe00db
bottkars <karsten.bott@dell.com>
2020-11-04 15:26:34 +0100

    script updated

--- ci/scripts/terraform-output.sh
+++ ci/scripts/terraform-output.sh
@@ -9,18 +9,27 @@ echo "Creating Output"
 cat << "EOF" > backend.tf
 terraform {
      backend "s3" {}
-     }
+}
 EOF
 
 echo "Initializing Backend" 
-terraform init 
+terraform init -input=false \
+    -backend-config=force_path_style=${TF_BACKEND_CONFIG_force_path_style} \
+    -backend-config=access_key=${TF_BACKEND_CONFIG_access_key} \
+    -backend-config=skip_region_validation=${TF_BACKEND_CONFIG_skip_region_validation} \
+    -backend-config=skip_metadata_api_check=true \
+    -backend-config=endpoint=${TF_BACKEND_CONFIG_endpoint} \
+    -backend-config=bucket=${TF_BACKEND_CONFIG_bucket} \
+    -backend-config=skip_credentials_validation=true \
+    -backend-config=key=${TF_BACKEND_CONFIG_key} \
+    -backend-config=secret_key=${TF_BACKEND_CONFIG_secret_key} \
+    -backend-config=region=${TF_BACKEND_CONFIG_region} .
+
 terraform output 
-
-
 
 timestamp="$(date '+%Y%m%d.%-H%M.%S')"
 export timestamp
 
+
 OUTPUT_FILE="$(echo "$TFSTATE" | envsubst)" 
 echo terraform output  >> terraform-output/${OUTPUT_FILE}
-
