a98dd2aebac130243e68c89ecf709399ee9ade97
bottkars <karsten.bott@dell.com>
2020-11-04 19:37:57 +0100

    figlet docker image bug

--- ci/scripts/terraform-show-plan.sh
+++ ci/scripts/terraform-show-plan.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 # [[ "${DEBUG}" == "TRUE" ]] && set -x
+apk add  gettext \
+      figlet > /dev/null 2>&1
+
 figlet DPS Automation
 echo "Creating Plan Environment"
 mv plan-output-archive/terraform/.t* plan-output-archive/terraform/${STATE_OUTPUT_DIR}/
