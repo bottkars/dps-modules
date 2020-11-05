3cd4772a45a996562d327083e6333fe5226efba6
bottkars <karsten.bott@dell.com>
2020-11-05 16:17:22 +0100

    added deploy azure aks

---
+++ ci/scripts/deploy-aks-azure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+[[ "${DEBUG}" == "TRUE" ]] && set -x
+figlet DPS Automation
+source dps-modules/ci/functions/ppdm_functions.sh
+
+
+if [[ -d ppdm-config ]]
+then
+    PPDM_CONFIG_VERSION=$(cat ./ppdm-config/version) 
+    echo "Found PPDM confiog file, evaluating Variables from vonfiguration Version ${PPDM_CONFIG_VERSION}"
+    eval "$(jq -r 'keys[] as $key | "export \($key)=\"\(.[$key].value)\""' ./ppdm-config/tf-output-${PPDM_CONFIG_VERSION}.json)"
+fi
+
+
+az aks create -g ${RESOURCE_GROUP} \
+ -n ${AKS_CLUSTER_NAME} \
+ --network-plugin azure \
+ -k 1.17.11 \
+ --aks-custom-headers EnableAzureDiskFileCSIDriver=true \
+ --subscription ${AZURE_SUBSCRIPTION_ID} \
+ --vnet-subnet-id "/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Network/virtualNetworks/${RESOURCE_GROUP}-virtual-network/subnets/${RESOURCE_GROUP}-aks-subnet"
+
+az aks get-credentials --resource-group ${RESOURCE_GROUP} --name ${AKS_CLUSTER_NAME}
+
