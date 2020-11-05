e4fadd8078ed3b02709479e9f8db81ed42e08c3e
bottkars <karsten.bott@dell.com>
2020-11-05 12:37:23 +0100

    deed sonme more :-)

--- ci/scripts/set-ppdm-inventory.sh
+++ ci/scripts/set-ppdm-inventory.sh
@@ -3,6 +3,16 @@ set -eu
 [[ "${DEBUG}" == "TRUE" ]] && set -x
 figlet DPS Automation
 source dps-modules/ci/functions/ppdm_functions.sh
+
+
+if [[ -d ppdm-config ]]
+then
+    PPDM_CONFIG_VERSION=$(cat ./ppdm-config/version) 
+    echo "Found PPDM confiog file, evaluating Variables from vonfiguration Version ${PPDM_CONFIG_VERSION}"
+    eval "$(jq -r 'keys[] as $key | "export \($key)=\"\(.[$key].value)\""' ./ppdm-config/tf-output-${PPDM_CONFIG_VERSION}.json)"
+    export INVENTORY_FQDN="${DDVE_FQDN}"
+fi
+
 echo "requesting API token"
 
 
--- ci/scripts/set-ppdm-sdr.sh
+++ ci/scripts/set-ppdm-sdr.sh
@@ -3,13 +3,19 @@ set -eu
 [[ "${DEBUG}" == "TRUE" ]] && set -x
 figlet DPS Automation
 
-echo "checking for jq...."
-DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
-source dps-modules/ci/functions/ppdm_functions.sh
+
+if [[ -d ppdm-config ]]
+then
+    PPDM_CONFIG_VERSION=$(cat ./ppdm-config/version) 
+    echo "Found PPDM confiog file, evaluating Variables from vonfiguration Version ${PPDM_CONFIG_VERSION}"
+    eval "$(jq -r 'keys[] as $key | "export \($key)=\"\(.[$key].value)\""' ./ppdm-config/tf-output-${PPDM_CONFIG_VERSION}.json)"
+fi
+echo
+
 echo "requesting API token"
 
 export PPDM_TOKEN=$(get_ppdm_token ${PPDM_PASSWORD})
 echo "Configuring Server DR"
-result=$(set_ppdm_sdr-settings "${PPDD_FQDN}" "${PPDD_PATH}" "DATA_DOMAIN_SYSTEM" "true")
+result=$(set_ppdm_sdr-settings "${PPDD_FQDN:-$PPDM_PRIVATE_FQDN}" "${PPDD_PATH}" "DATA_DOMAIN_SYSTEM" "true")
 echo $result 
 
