f4b8d06790969eee8ad3fea1fea6c607f5a7eda0
bottkars <karsten.bott@dell.com>
2020-11-10 16:44:43 +0100

    add k8s app

--- ci/scripts/deploy-mysql-k8s.sh
+++ ci/scripts/deploy-mysql-k8s.sh
@@ -11,24 +11,8 @@ KUBECONFIG_VERSION=$(cat ./kubeconfig/version)
 
 
 export KUBECONFIG=${PWD}/kubeconfig/kubeconfig-${KUBECONFIG_VERSION}.json
-
+echo "Creating MYSQL App in ${NAMESPACE}"
 kubectl create namespace ${MYSQL_NAMESPACE}
 kubectl apply -f ${MYSQL_SECRET} --namespace ${MYSQL_NAMESPACE}
 kubectl apply -f ${MYSQL_PVC} --namespace ${MYSQL_NAMESPACE}
 kubectl apply -f ${MYSQL_DEPLOYMENT} --namespace ${MYSQL_NAMESPACE}
-
-export PPDM_K8S_TOKEN=$(kubectl get secret "$(kubectl -n kube-system get secret | grep ppdm-admin | awk '{print $1}')" \
--n kube-system --template={{.data.token}} | base64 -d)
-
-
-
-echo "Calling Playbook ${PLAYBOOK}"
-
-ansible-playbook ${PLAYBOOK}
-
-timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
-
-PPDM_K8S_FILE=ppdmk8stoken-$timestamp.json
-
-PPDM_K8S_OUTPUT_FILE="$(echo "$PPDM_K8S_FILE" | envsubst '$timestamp')"
-echo $PPDM_K8S_TOKEN > k8stoken/${PPDM_K8S_OUTPUT_FILE}
