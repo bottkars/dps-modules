386ae742eff0d428c8114f40b1c3a702d4fd7fd0
bottkars <karsten.bott@dell.com>
2020-11-10 16:51:36 +0100

    mysql namespace

--- ci/scripts/deploy-mysql-k8s.sh
+++ ci/scripts/deploy-mysql-k8s.sh
@@ -11,7 +11,7 @@ KUBECONFIG_VERSION=$(cat ./kubeconfig/version)
 
 
 export KUBECONFIG=${PWD}/kubeconfig/kubeconfig-${KUBECONFIG_VERSION}.json
-echo "Creating MYSQL App in ${NAMESPACE}"
+echo "Creating MYSQL App in ${MYSQL_NAMESPACE}"
 kubectl create namespace ${MYSQL_NAMESPACE}
 kubectl apply -f ${MYSQL_SECRET} --namespace ${MYSQL_NAMESPACE}
 kubectl apply -f ${MYSQL_PVC} --namespace ${MYSQL_NAMESPACE}
