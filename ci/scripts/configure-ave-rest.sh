af2b26ab5b6c19dbf15d05a8f917ed5c01159a4b
bottkars <karsten.bott@dell.com>
2021-04-12 16:18:36 +0200

    contains

--- ci/scripts/configure-ave-rest.sh
+++ ci/scripts/configure-ave-rest.sh
@@ -63,7 +63,7 @@ data='{"timezone_name":"'${AVE_TIMEZONE}'",
 data=$(echo "${data}" | jq -c .)
 set_avi_config $data ave-config | jq -r .
 
-until  [[  $(get_avi_messages | jq -r 'select(.[-1].status == "completed")' 2> /dev/null) ]] || [[ $(get_avi_packages_messages | jq '.[] | select(.content) >= "region finished"' 2> /dev/null) == true  ]]
+until  [[  $(get_avi_messages | jq -r 'select(.[-1].status == "completed")' 2> /dev/null) ]] || [[ $(get_avi_messages | jq '.[] | select(.content) >= "region finished"' 2> /dev/null) == true  ]]
     do
     get_avi_messages  | jq -r  '.[-1] | [.timestamp, .status, .taskId, .taskName, .content] | @tsv'
     sleep 10
