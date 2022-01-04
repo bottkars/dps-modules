f651084e7477d44697226163b1a84fdc5d89ad5a
Karsten Bott <Karsten.Bott@bottnet.de>
2022-01-04 16:51:57 +0100

    Update ppdm-delete-vm-instant-access.sh

--- ci/scripts/ppdm-delete-vm-instant-access.sh
+++ ci/scripts/ppdm-delete-vm-instant-access.sh
@@ -2,7 +2,7 @@
 set -eu
 [[ "${DEBUG}" == "TRUE" ]] && set -x
 figlet DPS Automation
-DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
+# DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 source dps-modules/ci/functions/ppdm_functions.sh
 source dps-modules/ci/functions/yaml.sh
 
