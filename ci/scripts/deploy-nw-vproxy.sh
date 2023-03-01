d45e8b14b7e6d4264f6629ea6f32f2ccd9f9797b
bottkars <karsten.bott@dell.com>
2023-03-01 16:57:07 +0100

    vproxy

--- ci/scripts/deploy-nw-vproxy.sh
+++ ci/scripts/deploy-nw-vproxy.sh
@@ -16,7 +16,14 @@ echo "preparing vproxy ${VPROXY_VERSION} VPROXY"
 
 
 govc import.spec vproxy/vproxy-installer-${VPROXY_VERSION}.ova > vProxy.json
-echo "configuring appliance (vami) settings"
+
+
+
+
+
+
+
+
 jq  '(.PropertyMapping[] | select(.Key == "vami.DNS.vProxy") | .Value) |= env.VPROXY_DNS' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 jq  '(.PropertyMapping[] | select(.Key == "vami.searchDomain.vProxy") | .Value) |= env.VPROXY_SEARCHDOMAIN' vProxy.json > "tmp" && mv "tmp" vProxy.json
 jq  '(.PropertyMapping[] | select(.Key == "vami.fqdn.vProxy") | .Value) |= env.VPROXY_FQDN' vProxy.json  > "tmp" && mv "tmp" vProxy.json
@@ -29,19 +36,20 @@ jq  '(.PropertyMapping[] | select(.Key == "vami.netmask1.vProxy") | .Value) |= e
 jq  '(.PropertyMapping[] | select(.Key == "vami.gateway1.vProxy") | .Value) |= env.VPROXY_GATEWAY' vProxy.json > "tmp" && mv "tmp" vProxy.json
 jq  '(.PropertyMapping[] | select(.Key == "vami.timezone.vProxy") | .Value) |= env.VPROXY_TIMEZONE' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 jq  '(.PropertyMapping[] | select(.Key == "vami.NTP.vProxy") | .Value) |= env.VPROXY_NTP' vProxy.json  > "tmp" && mv "tmp" vProxy.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.rootpassword.vProxy") | .Value) |= env.VPROXY_ROOTPASSWORD' vProxy.json  > "tmp" && mv "tmp" vProxy.json
-jq  '(.PropertyMapping[] | select(.Key == "vami.adminpassword.vProxy") | .Value) |= env.VPROXY_ADMINPASSWORD' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.rootpassword.vProxy") | .Value) |= env.VPROXY_ROOT_PASSWORD' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.adminpassword.vProxy") | .Value) |= env.VPROXY_ADMIN_PASSWORD' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 
 jq  '(.PropertyMapping[] | select(.Key == "vami.FQDN.vProxy") | .Value) |= env.VPROXY_FQDN' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 jq  '(.NetworkMapping[0].Name |= env.VPROXY_NETWORK0)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 jq  '(.NetworkMapping[1].Name |= env.VPROXY_NETWORK1)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
-jq  '(.NetworkMapping[0].Network |= "ethernet-0")' vProxy.json  > "tmp" && mv "tmp" vProxy.json
-jq  '(.NetworkMapping[1].Network |= "ethernet-1")' vProxy.json  > "tmp" && mv "tmp" vProxy.json
-# jq  '(.PowerOn |= false)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.NetworkMapping[0].Network |= env.VPROXY_NETWORK0)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.NetworkMapping[1].Network |= env.VPROXY_NETWORK0)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.PowerOn |= false)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
+jq  '(.DiskProvisioning |= "thin")' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 jq  '(.InjectOvfEnv |= true)' vProxy.json  > "tmp" && mv "tmp" vProxy.json
 
 echo "importing vProxy ${VPROXY_VERSION} VPROXY template"
-govc import.ova -name ${VPROXY_VMNAME} -folder=${VPROXY_FOLDER}  -options=vProxy.json vProxy/VPROXY-${VPROXY_VERSION}.ova
+govc import.ova-name ${VPROXY_VMNAME} -folder=${VPROXY_FOLDER}  -options=vProxy.json vproxy/vproxy-installer-${VPROXY_VERSION}.ova
 govc vm.network.change -vm.ipath ${GOVC_VM_IPATH} -net=${VPROXY_NETWORK} ethernet-0
 
 govc vm.power -on=true -vm.ipath ${GOVC_VM_IPATH}
