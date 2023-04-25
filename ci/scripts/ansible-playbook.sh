065d316f4e89f5d642e9cd9e26fb1ecaa35dfdd3
bottkars <karsten.bott@dell.com>
2023-04-25 17:36:05 +0200

    latesr

--- ci/scripts/ansible-playbook.sh
+++ ci/scripts/ansible-playbook.sh
@@ -12,11 +12,13 @@ echo "Evaluating if ppdm config file is passed"
 
 if [[ -d ppdm-config ]]
 then
-    if [[ -f ppdm-config/version ]]
+    if [[ -f ppdm-config/version ]] 
+    then
         PPDM_CONFIG_VERSION=$(cat ./ppdm-config/version) 
         echo "Found PPDM config file, evaluating Variables from configuration Version ${PPDM_CONFIG_VERSION}"
         eval "$(jq -r 'keys[] as $key | "export \($key)=\"\(.[$key].value)\""' ./ppdm-config/tf-output-${PPDM_CONFIG_VERSION}.json)"
     elif [[ -f ppdm-config/metadata ]] 
+    then
         echo "found dps-terraform config metadata"   
         eval $(jq -r 'to_entries|map("export \(.key)=\"\(.value|tostring)\"")|.[]' ppdm-config/metadata)
     fi
